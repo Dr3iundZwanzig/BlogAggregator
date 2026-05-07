a4b371bf5be075c50500aa79fc129f67551a93a8
Dr3iundZwanzig <zaude.sven@gmail.com>
2026-05-07 20:27:32 +0200

    changed  argument handling in cli function for clarity

--- cli.go
+++ cli.go
@@ -7,16 +7,12 @@ import (
 
 func cli(st *state, commandsMap commands) error {
 	userInput := os.Args
-	userArguments := []string{}
 	if len(userInput) < 2 {
 		return fmt.Errorf("not enough arguments provided")
 	}
-	if len(userInput) > 2 {
-		userArguments = userInput[2:]
-	}
 	newCommand := command{
 		name:      userInput[1],
-		arguments: userArguments,
+		arguments: userInput[2:],
 	}
 	err := commandsMap.run(st, newCommand)
 	if err != nil {
